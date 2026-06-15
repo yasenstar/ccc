ffedf080f4106dc951aea0119b5a107bb6607983
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2026-06-15 13:45:29 +0800

    update J1 2026

--- junior/2026/J3/J3.py
+++ junior/2026/J3/J3.py
@@ -1,19 +1,30 @@
 Ngoc = input("All in capital, for Ngoc's candies: ")
 Minh = input("All in capitcal, for Minh's candies: ")
 
-print(len(Ngoc), len(Minh))
+n = []
+m = []
 
-compete_num = min(len(Ngoc), len(Minh))
+for i in Ngoc:
+    n.append(i)
+
+for i in Minh:
+    m.append(i)
+
+print(len(n), len(m))
+
+compete_num = min(len(n), len(m))
 
 score_n = 0
 score_m = 0
 
 for i in range(compete_num):
-    if Ngoc[i] == Minh[i]:
+    if n[i] == m[i]:
         score_n = score_n + 1
         score_m = score_m + 1
-        Ngoc.pop(i)
-        Minh.pop(i)
+        n.pop(i)
+        m.pop(i)
+        compete_num = min(len(n), len(m))
+        print(compete_num)
 
 print(Ngoc, score_n)
 
