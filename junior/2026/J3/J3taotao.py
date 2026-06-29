6a6b147156e7b9df394f78937ad690d69ce3c2db
Yichen <zhao.yichen@outlook.com>
2026-06-29 19:39:13 +0800

    J3taotao not complete

---
+++ junior/2026/J3/J3taotao.py
@@ -0,0 +1,8 @@
+n = input("Enter Ngoc candy: ")
+m = input("Enter Minh candy: ")
+if n[0] == "R" and m[0] == "G":
+    Ncandy + 1
+    elif n[0] == "G" and m[0] == "B":
+    Ncandy + 1
+    elif n[0] == "B" and m[0] == "R":
+    Ncandy + 1
